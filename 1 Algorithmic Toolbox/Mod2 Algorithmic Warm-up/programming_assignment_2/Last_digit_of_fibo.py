cd13245d7310c1328687329ed5e719fabe58c7f4
Kai Nguyen <s4126139@rmit.edu.vn>
2026-07-14 10:02:14 +0700

    finish 2.1.2

---
+++ 1 Algorithmic Toolbox/Mod2 Algorithmic Warm-up/programming_assignment_2/Last_digit_of_fibo.py
@@ -0,0 +1,11 @@
+def last_digit_of_fibo_array(n):
+    F=[0]*(n+1)
+    F[0],F[1]=0,1
+    for i in range(2,n+1):
+        F[i]=F[i-1]+F[i-2]
+    return F[n]%10
+
+n = int(input())
+print(last_digit_of_fibo_array(n))
+
+    
