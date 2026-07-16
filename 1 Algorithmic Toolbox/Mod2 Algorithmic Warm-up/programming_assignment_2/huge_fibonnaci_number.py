cd46fcd3b6fc5f77c00f318ae03dcfb553514338
Kai Nguyen <s4126139@rmit.edu.vn>
2026-07-16 14:53:57 +0700

    feat(algo): add a new algo for huge fibo

--- 1 Algorithmic Toolbox/Mod2 Algorithmic Warm-up/programming_assignment_2/huge_fibonnaci_number.py
+++ 1 Algorithmic Toolbox/Mod2 Algorithmic Warm-up/programming_assignment_2/huge_fibonnaci_number.py
@@ -1,10 +1,30 @@
-#O(n^2)-> out of memory
-def huge_fibonnaci_number(n,m):
-    F=[0]*(n+1)
-    F[0],F[1] = 0,1
-    for i in range(2,n+1):
-        F[i] = F[i-1] + F[i-2]
-    return F[n] % m
+# #O(n) time complexity and O(n) space complexity
+# # Time complexity: O(n^2) when counting big-integer addition cost
+# #F[i] has about Theta(i) digits, so addition at step i costs Theta(i)
+# def huge_fibonnaci_number(n,m):
+#     F=[0]*(n+1)
+#     F[0],F[1] = 0,1
+#     for i in range(2,n+1):
+#         F[i] = F[i-1] + F[i-2]
+#     return F[n] % m
+
+# #O(n) time complexity and O(1) space complexity
+# #Have to wait if n is so big
+# def huge_fibonnaci_number(n,m):
+#     if n <= 1:
+#         return n%m
+#     else:
+#         a, b = 0, 1
+#         for i in range(2,n+1):
+#             if i % 2 == 0:
+#                 a = (a+b)%m
+#             else:
+#                 b = (a+b)%m
+#     if n % 2 == 0:
+#         return a 
+#     else: return b
+
+
 
 n, m = map(int, input().split())
 print(huge_fibonnaci_number(n,m))
