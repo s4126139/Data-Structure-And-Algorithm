79034af55852f03d56cd4fa0d831973ed9eaeaa2
Kai Nguyen <s4126139@rmit.edu.vn>
2026-07-18 05:24:48 +0700

    finish front_back.py

---
+++ 0 pre dsa/Warmup-1/front_back.py
@@ -0,0 +1,12 @@
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+def front_back(str):
+    if len(str) <= 1:
+        return str
+    else:
+        n = len(str)-1
+        return str[n]+str[1:n]+str[0]
