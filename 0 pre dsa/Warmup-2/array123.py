2d71e833bca80d23adc9ff8b7b0a890730e18d20
Kai Nguyen <s4126139@rmit.edu.vn>
2026-07-19 10:27:16 +0700

    finish array123

---
+++ 0 pre dsa/Warmup-2/array123.py
@@ -0,0 +1,13 @@
+
+# Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+
+# array123([1, 1, 2, 3, 1]) → True
+# array123([1, 1, 2, 4, 1]) → False
+# array123([1, 1, 2, 1, 2, 3]) → True
+
+def array123(nums):
+    for i in range(len(nums)-2):
+        if [1,2,3] == [nums[i],nums[i+1],nums[i+2]]:
+            return True
+    return False
