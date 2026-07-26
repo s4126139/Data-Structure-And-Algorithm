4f067d2fac1c095d4421c51370d68d35785ff144
Kai Nguyen <s4126139@rmit.edu.vn>
2026-07-26 13:09:52 +0700

    finish make_chocolate.py

---
+++ 0 pre dsa/Logic-2/make_chocolate.py
@@ -0,0 +1,14 @@
+
+# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+
+# make_chocolate(4, 1, 9) → 4
+# make_chocolate(4, 1, 10) → -1
+# make_chocolate(4, 1, 7) → 2
+
+def make_chocolate(small, big, goal):
+    need_small = max(goal - 5*big,goal - goal//5*5)
+    if small < need_small:
+        return -1
+    else:
+        return need_small
