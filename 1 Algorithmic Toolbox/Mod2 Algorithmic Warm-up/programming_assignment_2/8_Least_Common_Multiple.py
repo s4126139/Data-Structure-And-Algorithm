fadbe2476a402c3e1989bd2741c52b6273d16415
Kai Nguyen <s4126139@rmit.edu.vn>
2026-08-11 11:47:43 +0700

    Finish 8_Least_Common_Multiple.py

---
+++ 1 Algorithmic Toolbox/Mod2 Algorithmic Warm-up/programming_assignment_2/8_Least_Common_Multiple.py
@@ -0,0 +1,10 @@
+def GCD(a,b):
+    while b != 0:
+        b, a = a%b, b
+    return a
+
+def LCM(a,b):
+    gcd_ab = GCD(a,b)
+    return int(a/gcd_ab)*int(b/gcd_ab)*gcd_ab
+
+print(LCM(761457,614573))
